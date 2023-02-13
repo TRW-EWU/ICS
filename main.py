cf6f9b4229eb47c5082c0d738ed7889c8ef80295
Thomas Walsh <twalsh@ewu.edu>
2023-02-13 13:39:02 -0800

    Option A...

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ def get_arguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-a", "--aaa", dest="aaa", help="This is A???")
 	arguments = parser.parse_args()
-	if not arguments.aaa:
-		parser.error("[-] Please specify arg 'aaa' .")
+	# if not arguments.aaa:
+		# parser.error("[-] Please specify arg 'aaa' .")
 	return arguments
 
 print("TIP ICS");
 
 options = get_arguments()
-aaa = options.aaa
-print(aaa)
+if options.aaa:
+	print(options.aaa)
+else:
+	print("Option aaa not entered")

9277732f630585fdfe74e04fdc19cd570db6874b
Thomas Walsh <twalsh@ewu.edu>
2023-02-13 13:22:33 -0800

    Argument -a

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import argparse
 
+def get_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-a", "--aaa", dest="aaa", help="This is A???")
+	arguments = parser.parse_args()
+	if not arguments.aaa:
+		parser.error("[-] Please specify arg 'aaa' .")
+	return arguments
 
-print("Hello World");
-print("Hello World");
-print("Hello World");
-print("Hello World");
-print("Hello World");
+print("TIP ICS");
+
+options = get_arguments()
+aaa = options.aaa
+print(aaa)

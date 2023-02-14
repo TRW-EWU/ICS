6a32d8f4d2b1dc5202e0214baf8a861fb575fddc
Thomas Walsh <twalsh@ewu.edu>
2023-02-14 08:58:24 -0800

    Modbus Server Stub

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env/python3
+
+from pymodbus.server import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
 import argparse
 
 def get_arguments():
@@ -15,3 +22,23 @@ if options.aaa:
 	print(options.aaa)
 else:
 	print("Option aaa not entered")
+
+print("Modbus Server ....")
+
+store = ModbusSlaveContext(
+   di = ModbusSequentialDataBlock(0, [17]*100), # digital inputs
+   co = ModbusSequentialDataBlock(0, [17]*100), # coil outputs
+   hr = ModbusSequentialDataBlock(0, [17]*100), # holding registers
+   ir = ModbusSequentialDataBlock(0, [17]*100)) # ????
+
+context = ModbusServerContext(slaves=store, single=True)
+
+identity = ModbusDeviceIdentification()
+identity.VendorName = 'PyModbus Inc.'
+identity.ProductCode = 'PM'
+identity.VendorUrl = 'https://github.com/riptideio/pyModbus'
+identity.ProductName = 'Modbus Server'
+identity.ModelName = 'PyModbus'
+identity.MajorMinorRevision = '1.0'
+
+StartTcpServer(context=context, identity=identity, address=("0.0.0.0", 502))

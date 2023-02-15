44f026b850cf1ae6d70583bf4969518dcc8d9eb8
Thomas Walsh <twalsh@ewu.edu>
2023-02-15 11:42:56 -0800

    Tip Initial Merge

---
+++ tip.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env/python3
+
+##################################################
+# TIP
+#
+
+import argparse
+from MyModbusServer import MyModbusServer
+
+def get_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--server", dest="server", help="SERVER: modbus, profinet, s7")
+	arguments = parser.parse_args()
+	# if not arguments.aaa:
+		# parser.error("[-] Please specify arg 'aaa' .")
+	return arguments
+
+################### Main #######################
+
+def main():
+	options = get_arguments()
+	if options.server == 'modbus':
+		svr = MyModbusServer()
+		svr.modbus_server()
+	else:
+		print("*** Invalid server option: " + options.server)
+
+if __name__ == '__main__':
+	main()
+
+
+

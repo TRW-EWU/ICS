25f465bc471af9da091d243918e8c4f88e93a55e
Thomas Walsh <twalsh@ewu.edu>
2023-02-15 10:06:56 -0800

    Modbus Server Code

---
+++ MyModbusServer.py
@@ -0,0 +1,41 @@
+
+
+from pymodbus.server import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+#
+# modbus-cli
+#
+# modbus read <IP> %M1 -5
+# modbus read <IP> 300001 1-10
+# modbus read <IP> 400001 1-10
+#
+
+class MyModbusServer:
+    def __init__(self):
+        self.coil_out = ModbusSequentialDataBlock(0, [1, 0, 0, 1, 1, 0, 1, 1, 1]) # %M1 5
+        self.holding_reg = ModbusSequentialDataBlock(0, [1,2,3,4,5,6,8]*10)       # 40 0001
+        self.input_reg = ModbusSequentialDataBlock(0, [9, 0]*100)                 # 30 0001
+
+    def modbus_server(self):
+        print("Modbus Server ....")
+        self.store = ModbusSlaveContext(
+            di = ModbusSequentialDataBlock(0, [17]*100), # digital inputs
+            co = self.coil_out,
+            hr = self.holding_reg,
+            ir = self.input_reg)
+        self.context = ModbusServerContext(slaves=self.store, single=True)
+
+        self.identity = ModbusDeviceIdentification()
+        self.identity.VendorName = 'PyModbus Inc.'
+        self.identity.ProductCode = 'PM'
+        self.identity.VendorUrl = 'https://github.com/riptideio/pyModbus'
+        self.identity.ProductName = 'Modbus Server'
+        self.identity.ModelName = 'PyModbus'
+        self.identity.MajorMinorRevision = '1.0'
+
+        self.svr = StartTcpServer(context=self.context, identity=self.identity, 
+                                    address=("0.0.0.0", 502))
+
--- main.py
+++ main.py
@@ -1,44 +1,28 @@
 #!/usr/bin/env/python3
 
-from pymodbus.server import StartTcpServer
-from pymodbus.device import ModbusDeviceIdentification
-from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
-
 import argparse
+from MyModbusServer import MyModbusServer
 
 def get_arguments():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-a", "--aaa", dest="aaa", help="This is A???")
+	parser.add_argument("-s", "--server", dest="server", help="SERVER: modbus, profinet, s7")
 	arguments = parser.parse_args()
 	# if not arguments.aaa:
 		# parser.error("[-] Please specify arg 'aaa' .")
 	return arguments
 
-print("TIP ICS");
+################### Main #######################
 
-options = get_arguments()
-if options.aaa:
-	print(options.aaa)
-else:
-	print("Option aaa not entered")
+def main():
+	options = get_arguments()
+	if options.server == 'modbus':
+		svr = MyModbusServer()
+		svr.modbus_server()
+	else:
+		print("*** Invalid server option: " + options.server)
 
-print("Modbus Server ....")
+if __name__ == '__main__':
+	main()
 
-store = ModbusSlaveContext(
-   di = ModbusSequentialDataBlock(0, [17]*100), # digital inputs
-   co = ModbusSequentialDataBlock(0, [17]*100), # coil outputs
-   hr = ModbusSequentialDataBlock(0, [17]*100), # holding registers
-   ir = ModbusSequentialDataBlock(0, [17]*100)) # ????
 
-context = ModbusServerContext(slaves=store, single=True)
 
-identity = ModbusDeviceIdentification()
-identity.VendorName = 'PyModbus Inc.'
-identity.ProductCode = 'PM'
-identity.VendorUrl = 'https://github.com/riptideio/pyModbus'
-identity.ProductName = 'Modbus Server'
-identity.ModelName = 'PyModbus'
-identity.MajorMinorRevision = '1.0'
-
-StartTcpServer(context=context, identity=identity, address=("0.0.0.0", 502))

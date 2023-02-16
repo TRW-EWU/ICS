875cb9a2eda46529d6d20178649cb9c0c030829a
Thomas Walsh <twalsh@ewu.edu>
2023-02-16 14:50:02 -0500

    MyModbusClient more work

--- MyModbusClient.py
+++ MyModbusClient.py
@@ -1,15 +1,15 @@
-From scapy.all import *
+from scapy.all import *
 from Modbus import *
 import time
 
 # Defining the script variables
 srcIP = '192.168.1.1'
-srcPort = random.randit(1024, 65535)
+srcPort = random.randint(1024, 65535)
 dstIP = '192.168.1.2'
 dstPort = 502
-seqNr = random.randit(444, 8765432)
+seqNr = random.randint(444, 8765432)
 ackNr = 0
-transID = random.randit(44, 44444)
+transID = random.randint(44, 44444)
 
 def updateSeqAndAckNrs(sendPkt, recdPkt):
     # Keep track of tcp sequence and acknowledge numbers
@@ -19,6 +19,12 @@ def updateSeqAndAckNrs(sendPkt, recdPkt):
 
 def main():
     print("MyModbusClient")
+
+
+class MyModbusClient:
+    def __init__(self):
+        self.x = 4
+
 
 if __name__ == '__main__':
 	main()
--- tip.py
+++ tip.py
@@ -2,10 +2,12 @@
 
 import argparse
 from MyModbusServer import MyModbusServer
+from MyModbusClient import MyModbusClient
 
 def get_arguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-s", "--server", dest="server", help="SERVER: modbus, profinet, s7")
+	parser.add_argument("-c", "--client", dest="client", help="CLIENT: modbus, profinet, s7")
 	arguments = parser.parse_args()
 	# if not arguments.aaa:
 		# parser.error("[-] Please specify arg 'aaa' .")
@@ -15,11 +17,20 @@ def get_arguments():
 
 def main():
 	options = get_arguments()
-	if options.server == 'modbus':
-		svr = MyModbusServer()
-		svr.modbus_server()
-	else:
-		print("*** Invalid server option: " + options.server)
+	#print(options)
+	#print(options.server)
+	#print(options.client)
+
+	if (options.server != None):
+		if (options.server == 'modbus'):
+			svr = MyModbusServer()
+			svr.modbus_server()
+
+	if (options.client != None):
+		if (options.client == 'modbus'):
+			svr = MyModbusClient()
+			print(svr.x)
+			#svr.modbus_client()
 
 if __name__ == '__main__':
 	main()
